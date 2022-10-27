2019836afe3d58103f80175dbb689aeae4482e5a
ivanamoros <ivan13ap@gmail.com>
2022-10-27 20:46:28 +0200

    first commit

---
+++ src/Warrior.java
@@ -0,0 +1,60 @@
+public class Warrior extends Character implements Attacker{
+
+    private int stamina;
+    private int strength;
+
+    // OVERRIDE - Set HP (Limits 1-100)
+    @Override
+    public void setHp(int hp) {
+        if (hp>100){
+            super.setHp(100);
+        }else if (hp<1){
+            super.setHp(1);
+        }
+        else{
+            super.setHp(hp);
+        }
+    }
+    public Warrior(int id, String name, int hp, int stamina, int strength) {
+        super(id, name, hp);
+        setStamina(stamina);
+        setStrength(strength);
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    // Set Stamina (Limits 10-50)
+    public void setStamina(int stamina) {
+        if (stamina>50){
+            this.stamina=50;
+        }else if (stamina<10){
+            this.stamina=10;
+        }
+        else{
+            this.stamina=stamina;
+        }
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    // Set Strength (Limits 1-10)
+    public void setStrength(int strength) {
+        if (strength>10){
+            this.strength=10;
+        }else if (strength<1){
+            this.strength=1;
+        }
+        else{
+            this.strength=strength;
+        }
+    }
+
+    @Override
+    public void attack() {
+
+    }
+}
---
+++ src/Wizard.java
@@ -0,0 +1,47 @@
+public class Wizard extends Character implements Attacker{
+
+    private int mana;
+    private int intelligence;
+
+    //Constructor
+
+    public Wizard(int id, String name, int hp, int mana, int intelligence) {
+        super(id, name, hp);
+        setMana(mana);
+        setIntelligence(intelligence);
+    }
+
+    //Getters & Setters
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        if (mana>=10){mana=10;}
+        else if (mana<=50){mana=50;}
+        else {mana= mana;}
+    }
+
+    @Override
+    public void setHp(int hp) {
+       if (hp > 50){ super.setHp(50);}
+       else if (hp < 1){ super.setHp(1);}
+       else  {super.setHp(hp);}
+    }
+
+    public int getIntelligence() {
+         return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        if (intelligence>=1){intelligence=1;}
+        else if (intelligence<=50){intelligence=50;}
+        else {intelligence= intelligence;}
+    }
+
+    @Override
+    public void attack() {
+
+    }
+}
