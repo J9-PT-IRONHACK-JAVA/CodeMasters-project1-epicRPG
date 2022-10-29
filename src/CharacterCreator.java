c3084a5af50b26a858c2528f64860199d1c7875e
iamoros- <ivan13ap@gmail.com>
2022-10-29 02:53:53 +0200

    manual character and 3 random characters implementation

--- src/CharacterCreator.java
+++ src/CharacterCreator.java
@@ -5,43 +5,100 @@ public class CharacterCreator {
 
     public static void menu(){
 
-//        String command = "start";
-//
-//        while (command.equals("exit") == false){
-//            System.out.println();
-//        }
-
         int character;
         int characters;
+
         var scan = new Scanner(System.in);
         var team1 = new ArrayList<Character>();
         var team2 = new ArrayList<Character>();
 
         System.out.println("How many characters do you want for each team:");
         characters = scan.nextInt();
+        scan.nextLine();
+        System.out.println("\nNICE! You are going to fight "+characters+" vs "+characters+"\n");
+        System.out.println("TIME TO SET THE TEAM 1:");
         for (int i = 0; i < characters; i++) {
-            System.out.println("\nCharacter " + i + ".\n 1. Warrior or 2. Wizard?");
+            System.out.println("\nCHARACTER "+(i+1)+".\n 1. Warrior or 2. Wizard?");
             character = 0;
             while (character != 1 || character != 2) {
                 character = scan.nextInt();
+                scan.nextLine();
+                if (character != 1 && character != 2){
+                    System.out.println("Bad input! Write 1 or 2 and press intro.");
+                }
+                if (character == 1) {
+                    warriorCreator(scan, team1, i);
+                    break;
+                } else if (character == 2) {
+                    wizardCreator(scan, team1, i);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("\n\nTIME TO SET THE TEAM 2:");
+        for (int i = 0; i < characters; i++) {
+            System.out.println("\nCHARACTER " +(i+1)+ ".\n 1. Warrior or 2. Wizard?");
+            character = 0;
+            while (character != 1 || character != 2) {
+                character = scan.nextInt();
+                scan.nextLine();
                 if (character != 1 && character != 2){
                     System.out.println("Bad input!");
                 }
                 if (character == 1) {
-                    team1.add(new Warrior(i, "ivan", 50, 100, 50));
-                } else {
-                    team1.add(new Wizard(i, "salva", 50, 100, 50));
+                    warriorCreator(scan, team2, i);
+                    break;
+                } else if (character == 2) {
+                    wizardCreator(scan, team2, i);
+                    break;
                 }
             }
         }
 
 
+    }
 
+    private static void warriorCreator(Scanner scan, ArrayList<Character> team, int i) {
 
+        String name;
+        int stamina;
+        int strength;
+        int hp;
 
-        for (int i = 0; i < characters; i++) {
-            team2.add(new Wizard(i, "salva", 50, 100, 50));
-        }
+        System.out.println("\nName for the Warrior:");
+        name = scan.nextLine();
+        System.out.println("\nSet the HP between 100 and 200:");
+        hp = scan.nextInt();
+        scan.nextLine();
+        System.out.println("\nSet the stamina between 10 and 50:");
+        stamina = scan.nextInt();
+        scan.nextLine();
+        System.out.println("\nSet the strength between 1 and 10:");
+        strength = scan.nextInt();
+        scan.nextLine();
+        team.add(new Warrior(i, name, hp, stamina, strength));
+    }
+
+    private static void wizardCreator(Scanner scan, ArrayList<Character> team1, int i) {
+
+        String name;
+        int mana;
+        int intelligence;
+        int hp;
+
+        System.out.println("\nName for the Wizard:");
+        name = scan.nextLine();
+        System.out.println("\nSet the HP between 50 and 100:");
+        hp = scan.nextInt();
+        scan.nextLine();
+        System.out.println("\nSet the mana between 10 and 50:");
+        mana = scan.nextInt();
+        scan.nextLine();
+        System.out.println("\nSet the intelligence between 1 and 50:");
+        intelligence = scan.nextInt();
+        scan.nextLine();
+        team1.add(new Warrior(i, name, hp, mana, intelligence));
     }
 
 }
---
+++ src/CharacterThreeRandom.java
@@ -0,0 +1,144 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CharacterThreeRandom {
+
+    public static void menu(){
+
+        int character;
+        int characters;
+
+        var scan = new Scanner(System.in);
+        var team1 = new ArrayList<Character>();
+        var team2 = new ArrayList<Character>();
+
+        System.out.println("How many characters do you want for each team:");
+        characters = scan.nextInt();
+        scan.nextLine();
+        System.out.println("\nNICE! You are going to fight "+characters+" vs "+characters+"\n");
+        System.out.println("TIME TO SET THE TEAM 1:");
+        for (int i = 0; i < characters; i++) {
+            System.out.println("\nCHARACTER "+(i+1)+".\n 1. Warrior or 2. Wizard?");
+            character = 0;
+            while (character != 1 || character != 2) {
+                character = scan.nextInt();
+                scan.nextLine();
+                if (character != 1 && character != 2){
+                    System.out.println("Bad input! Write 1 or 2 and press intro.");
+                }
+                if (character == 1) {
+                    threeRandomWarriors(scan, team1, i);
+                    break;
+                } else if (character == 2) {
+                    threeRandomWizards(scan, team1, i);
+                    break;
+                }
+            }
+        }
+        System.out.println("\n\nTIME TO SET THE TEAM 2:");
+        for (int i = 0; i < characters; i++) {
+            System.out.println("\nCHARACTER "+(i+1)+".\n 1. Warrior or 2. Wizard?");
+            character = 0;
+            while (character != 1 || character != 2) {
+                character = scan.nextInt();
+                scan.nextLine();
+                if (character != 1 && character != 2){
+                    System.out.println("Bad input! Write 1 or 2 and press intro.");
+                }
+                if (character == 1) {
+                    threeRandomWarriors(scan, team2, i);
+                    break;
+                } {
+                    threeRandomWizards(scan, team2, i);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void threeRandomWarriors(Scanner scan, ArrayList<Character> team, int i){
+
+        String selected = "test";
+        String[] namesList = {"Federico", "Patricia", "Andres", "Drina", "Ivan", "Valerio"};
+        String name1 = namesList[0 + (int)(Math.random() * ((5 - 0) + 1))];
+        String name2 = namesList[0 + (int)(Math.random() * ((5 - 0) + 1))];
+        String name3 = namesList[0 + (int)(Math.random() * ((5 - 0) + 1))];
+
+        int stamina1 = 10 + (int)(Math.random() * ((50 - 10) + 1));
+        int stamina2 = 10 + (int)(Math.random() * ((50 - 10) + 1));
+        int stamina3 = 10 + (int)(Math.random() * ((50 - 10) + 1));
+
+        int strength1 = 1 + (int)(Math.random() * ((10 - 1) + 1));
+        int strength2 = 1 + (int)(Math.random() * ((10 - 1) + 1));
+        int strength3 = 1 + (int)(Math.random() * ((10 - 1) + 1));
+
+        int hp1 = 100 + (int)(Math.random() * ((200 - 100) + 1));
+        int hp2 = 100 + (int)(Math.random() * ((200 - 100) + 1));
+        int hp3 = 100 + (int)(Math.random() * ((200 - 100) + 1));
+
+        System.out.println("You have to choose between these three warriors:\n");
+        System.out.println("Name:    \t"+name1+"\t\t"+name2+"\t\t"+name3);
+        System.out.println("Stamina: \t"+stamina1+"\t\t\t"+stamina2+"\t\t\t"+stamina3);
+        System.out.println("Strength:\t"+strength1+"\t\t\t"+strength2+"\t\t\t"+strength3);
+        System.out.println("HP:      \t"+hp1+"\t\t\t"+hp2+"\t\t\t"+hp3);
+        System.out.println("\nName of your chosen warrior:");
+        while (!selected.equals(name1) || !selected.equals(name2) || !selected.equals(name3)){
+            selected = scan.nextLine();
+            if (selected.equals(name1)){
+                team.add(new Warrior(i, name1, hp1, stamina1, strength1));
+                break;
+            } else if (selected.equals(name2)){
+                team.add(new Warrior(i, name2, hp2, stamina2, strength2));
+                break;
+            } else if (selected.equals(name3)) {
+                team.add(new Warrior(i, name3, hp3, stamina3, strength3));
+                break;
+            } else {
+                System.out.println("Wrong name!");
+            }
+        }
+    }
+
+    public static void threeRandomWizards(Scanner scan, ArrayList<Character> team, int i){
+
+        String selected = "test";
+        String[] namesList = {"Federico", "Patricia", "Andres", "Drina", "Ivan", "Valerio"};
+        String name1 = namesList[0 + (int)(Math.random() * ((5 - 0) + 1))];
+        String name2 = namesList[0 + (int)(Math.random() * ((5 - 0) + 1))];
+        String name3 = namesList[0 + (int)(Math.random() * ((5 - 0) + 1))];
+
+        int mana1 = 10 + (int)(Math.random() * ((50 - 10) + 1));
+        int mana2 = 10 + (int)(Math.random() * ((50 - 10) + 1));
+        int mana3 = 10 + (int)(Math.random() * ((50 - 10) + 1));
+
+        int intelligence1 = 1 + (int)(Math.random() * ((50 - 1) + 1));
+        int intelligence2 = 1 + (int)(Math.random() * ((50 - 1) + 1));
+        int intelligence3 = 1 + (int)(Math.random() * ((50 - 1) + 1));
+
+        int hp1 = 100 + (int)(Math.random() * ((200 - 100) + 1));
+        int hp2 = 100 + (int)(Math.random() * ((200 - 100) + 1));
+        int hp3 = 100 + (int)(Math.random() * ((200 - 100) + 1));
+
+        System.out.println("You have to choose between these three wizards:\n");
+        System.out.println("Name:\t\t"+name1+"\t"+name2+"\t"+name3);
+        System.out.println("Mana:\t"+mana1+"\t\t"+mana2+"\t\t"+mana3);
+        System.out.println("Intelligence:\t"+intelligence1+"\t\t\t"+intelligence2+"\t\t\t"+intelligence3);
+        System.out.println("HP:\t\t\t"+hp1+"\t\t\t"+hp2+"\t\t\t"+hp3);
+        System.out.println("\nName of your chosen wizard:");
+        while (!selected.equals(name1) || !selected.equals(name2) || !selected.equals(name3)){
+            selected = scan.nextLine();
+            if (selected.equals(name1)){
+                team.add(new Wizard(i, name1, hp1, mana1, intelligence1));
+                break;
+            } else if (selected.equals(name2)){
+                team.add(new Wizard(i, name2, hp2, mana2, intelligence2));
+                break;
+            } else if (selected.equals(name3)) {
+                team.add(new Wizard(i, name3, hp3, mana3, intelligence2));
+                break;
+            } else {
+                System.out.println("Wrong name!");
+            }
+        }
+    }
+}
