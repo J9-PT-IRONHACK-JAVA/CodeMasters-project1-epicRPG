4a17ab8bc6f6f6bbbfcaa85f488aa38e2b92bb3b
o-andres-m <elhinchon@gmail.com>
2022-10-29 12:45:06 +0200

    remove iml fies

---
+++ src/Csv.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Csv {
+
+
+    public void meunCsv(Scanner scanner){
+        System.out.println("Please put the name of CSV file:");
+        String filename = scanner.nextLine();
+        File file = new File(filename);
+
+
+
+
+    }
+
+}
