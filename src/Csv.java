01ed1d876afcd519eed9d417a797472969f57fe9
o-andres-m <elhinchon@gmail.com>
2022-10-29 15:46:02 +0200

    csv Final

--- src/Csv.java
+++ src/Csv.java
@@ -8,35 +8,48 @@ public class Csv {
     private ArrayList<Character> team = new ArrayList<Character>();
 
     public void meunCsv(Scanner scanner) throws FileNotFoundException {
-        System.out.println("Please put the name of CSV file:");
-        String filename = scanner.nextLine();
-        File file = new File(filename);
-        Scanner reader = new Scanner(file);
         ArrayList<String> fileList = new ArrayList<>();
-        while (reader.hasNextLine()){
-            fileList.add(reader.nextLine());
-        }
-        System.out.println(fileList.get(1).split(",")[0]);
+        boolean flag = false;
+        String filename;
+        do {
+            System.out.println("Please put the name of CSV file:");
+            filename = scanner.nextLine();
+            try {
+                File file = new File(filename);
+                Scanner reader = new Scanner(file);
+                while (reader.hasNextLine()) {
+                    fileList.add(reader.nextLine());
+                }
+                flag = true;
 
-        for (int i = 0; i < fileList.size(); i++) {
-            if (i!=0){
-                System.out.println(fileList.get(i));
+            } catch (Exception e) {
+                System.out.println("File not found, try again...");
             }
+        }while (flag == false);
 
+        try {
+            for (int i = 0; i < fileList.size(); i++) {
+                String[] line = fileList.get(i).split(",");
+                if (i != 0) {
+                     if (line[0].equals("WA")) {
+                         team.add(new Warrior(i + 1, line[1], Integer.parseInt(line[2]), Integer.parseInt(line[3]), Integer.parseInt(line[4])));
+                     } else {
+                         team.add(new Wizard(i + 1, line[1], Integer.parseInt(line[2]), Integer.parseInt(line[3]), Integer.parseInt(line[4])));
+                     }
+                    }
+                }
+            System.out.println("CHARACTERES CREADOS CORRECTAMENTE! -- ELIMINAR ESTA LINEA ");
+        } catch (Exception e) {
+            System.out.println("Error... Verify CSV file Format...");
         }
 
-        /*
-        var char1 = new Warrior(id,name,hp,stamina,stregnth)
-        var char2 = new Wizard(id,name,hp,stamina,stregnth)
-        */
-
-        for (int i = 0; i < fileList.size(); i++) {
-            System.out.println(fileList.get(i));
-
-        }
-
-
-
+        System.out.println("Aqui implementar lo siguiente, teniendo en cuenta:");
+        System.out.println("1) SI DA ERROR: ");
+        System.out.println("   - Volver al menu anterior?\n" +
+                           "   - pedir nuevamente el nobmre del archivo csv?\n" +
+                           "   - generar un random?");
+        System.out.println("2) SI NO DA ERROR Y CREA LOS CHARACTER SIN PROBLEMA:");
+        System.out.println("   - retornar la lista?\n");
 
     }
 
