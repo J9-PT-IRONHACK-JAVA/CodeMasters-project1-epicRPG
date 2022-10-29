9c411df83d3aa2ac70e029fc675e278b39e85a6d
DrinaLuburich <drina_lf93@hotmail.com>
2022-10-29 17:38:48 +0200

    add randomizer inside of team1&team2 (with errors)

--- src/randomTeam.java
+++ src/randomTeam.java
@@ -14,7 +14,7 @@ public class randomTeam {
         System.out.println("Press any key to generate a random team");
         System.in.read();
         int numberOfRandomPlayers = (2 + (int) (Math.random() * ((20 - 2) + 1)));
-        System.out.println("You are going to fight " + numberOfRandomPlayers + "vs. " + numberOfRandomPlayers+". Press any key to continue. ");
+        System.out.println("You are going to fight " + numberOfRandomPlayers + "vs. " + numberOfRandomPlayers + ". Press any key to continue. ");
         System.in.read();
 
         // ratio warrior:wizard aleatorio
@@ -22,33 +22,31 @@ public class randomTeam {
         ArrayList team1 = new ArrayList();
         ArrayList team2 = new ArrayList();
 
-        // 1º clase  - 2º nombre - 3º hp - 4º stamia/mana - 5º fuerza/inteligencia
+
+        //* PENDIENTE CREAR CLASES DE TEAM PARA AÑADIR OBJETOS *\\
         // Add to Team 1
-        for(int i=0;i<=numberOfRandomPlayers;i++){
-            switch (classDice){
+        // status randomizadas dentro de un rango
+        for (int i = 0; i <= numberOfRandomPlayers; i++) {
+            switch (classDice) {
                 case 1:
-                    team1.add("Wizard","RandomName1",(50 + (int) (Math.random() * ((100 - 50) + 1))),(10 + (int) (Math.random() * ((50 - 10) + 1))),(1 + (int) (Math.random() * ((50 - 1) + 1)));
+                    // 1º Clase  - 2º Nombre - 3º HP - 4º Stamina/mana - 5º Fuerza/inteligencia
+                    team1.add("Wizard", "RandomName1", (50 + (int) (Math.random() * ((100 - 50) + 1))), (10 + (int) (Math.random() * ((50 - 10) + 1))), (1 + (int) (Math.random() * ((50 - 1) + 1))));
                     break;
                 case 2:
-                    team1.add("Warrior","RandomName2",(100 + (int) (Math.random() * ((200 - 100) + 1))),(10 + (int) (Math.random() * ((50 - 10) + 1))),(1 + (int) (Math.random() * ((10 - 1) + 1)));
-                    break;
-            }}
-        // Add to Team 2
-        for(int i=0;i<=numberOfRandomPlayers;i++){
-            switch (classDice){
-                case 1:
-                    team2.add("Wizard","RandomName1",(50 + (int) (Math.random() * ((100 - 50) + 1))),(10 + (int) (Math.random() * ((50 - 10) + 1))),(1 + (int) (Math.random() * ((50 - 1) + 1)));
-                    break;
-                case 2:
-                    team2.add("Warrior","RandomName2",(100 + (int) (Math.random() * ((200 - 100) + 1))),(10 + (int) (Math.random() * ((50 - 10) + 1))),(1 + (int) (Math.random() * ((10 - 1) + 1)));
+                    team1.add("Warrior", "RandomName2", (100 + (int) (Math.random() * ((200 - 100) + 1))), (10 + (int) (Math.random() * ((50 - 10) + 1))), (1 + (int) (Math.random() * ((10 - 1) + 1))));
                     break;
             }
         }
-
-
-
-
-        // status randomizadas dentro de un rango
-
+        // Add to Team 2
+        for (int i = 0; i <= numberOfRandomPlayers; i++) {
+            switch (classDice) {
+                case 1:
+                    team2.add("Wizard", "RandomName1", (50 + (int) (Math.random() * ((100 - 50) + 1))), (10 + (int) (Math.random() * ((50 - 10) + 1))), (1 + (int) (Math.random() * ((50 - 1) + 1))));
+                    break;
+                case 2:
+                    team2.add("Warrior", "RandomName2", (100 + (int) (Math.random() * ((200 - 100) + 1))), (10 + (int) (Math.random() * ((50 - 10) + 1))), (1 + (int) (Math.random() * ((10 - 1) + 1))));
+                    break;
+            }
+        }
     }
 }
