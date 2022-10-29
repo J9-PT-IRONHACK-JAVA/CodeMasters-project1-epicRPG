4d606dd124f00be4919c947f9bae5b1ead8e43dc
DrinaLuburich <drina_lf93@hotmail.com>
2022-10-29 13:03:52 +0200

    first commit random Team

---
+++ src/randomTeam.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class randomTeam {
+    // Crear partidos completamente random
+    // nº jugadores random entre 2 y 20
+
+    int numberOfRandomPlayers = (2 + (int)(Math.random() * ((20-2)+1)));
+    System.out.println("You are going to fight " + numberOfRandomPlayers + "vs. " + numberOfRandomPlayers);
+
+    // ratio warrior:wizard aleatorio
+    int dice = (2+(int)(Math.random()*((2-1)+1)));
+
+    ArrayList<Character> team1 = new ArrayList<Character>();
+    team1.add(new)
+    ArrayList<Character> team2 = new ArrayList<Character>();
+
+    // status randomizadas dentro de un rango
+
+
+
+}
