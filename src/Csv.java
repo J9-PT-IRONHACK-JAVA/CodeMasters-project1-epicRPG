0ef3f8ee10d92e971553959d7c78c6afab6377d1
o-andres-m <elhinchon@gmail.com>
2022-10-29 13:04:01 +0200

    csv pruebas

--- src/Csv.java
+++ src/Csv.java
@@ -1,13 +1,38 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Csv {
 
+    private ArrayList<Character> team = new ArrayList<Character>();
 
-    public void meunCsv(Scanner scanner){
+    public void meunCsv(Scanner scanner) throws FileNotFoundException {
         System.out.println("Please put the name of CSV file:");
         String filename = scanner.nextLine();
         File file = new File(filename);
+        Scanner reader = new Scanner(file);
+        ArrayList<String> fileList = new ArrayList<>();
+        while (reader.hasNextLine()){
+            fileList.add(reader.nextLine());
+        }
+
+        for (int i = 0; i < fileList.size(); i++) {
+            if (i!=0){
+                System.out.println(fileList.get(i));
+            }
+
+        }
+
+        /*
+        var char1 = new Warrior(id,name,hp,stamina,stregnth)
+        var char2 = new Wizard(id,name,hp,stamina,stregnth)
+        */
+
+        for (int i = 0; i < fileList.size(); i++) {
+            System.out.println(fileList.get(i));
+
+        }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        var csv = new Csv();
+        Scanner scanner = new Scanner(System.in);
+        csv.meunCsv(scanner);
       
     }
 }
